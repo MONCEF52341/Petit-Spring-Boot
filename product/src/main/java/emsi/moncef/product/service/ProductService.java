6c06237bacef55c24ec00fbe70bba69098e079b3
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-25 20:33:09 +0100

    j'ai créé le service pour le modèle produit

---
+++ product/src/main/java/emsi/moncef/product/service/ProductService.java
@@ -0,0 +1,46 @@
+package emsi.moncef.product.service;
+
+
+import emsi.moncef.product.dto.ProductRequest;
+import emsi.moncef.product.dto.ProductResponse;
+import emsi.moncef.product.model.Product;
+import emsi.moncef.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .skuCode(productRequest.skuCode())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product created successfully");
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(),
+                product.getSkuCode(),
+                product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(),
+                        product.getSkuCode(),
+                        product.getPrice()))
+                .toList();
+    }
+}
