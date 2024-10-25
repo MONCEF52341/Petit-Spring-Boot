ba0935b2c828ff4625b8c84dfffd8dc3e3126777
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-25 20:33:03 +0100

    j'ai créé le controleur pour le modèle produit

---
+++ product/src/main/java/emsi/moncef/product/controller/ProductController.java
@@ -0,0 +1,33 @@
+package emsi.moncef.product.controller;
+
+import emsi.moncef.product.dto.ProductRequest;
+import emsi.moncef.product.dto.ProductResponse;
+import emsi.moncef.product.model.Product;
+import emsi.moncef.product.service.ProductService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
