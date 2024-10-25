069dd9dfe8a07eb60ef447906249a20c8b97727a
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-25 22:17:03 +0100

    J'ai rajouté le code initial du MS pour la gestion des commandes

---
+++ order/src/test/java/emsi/moncef/order/TestOrderApplication.java
@@ -0,0 +1,11 @@
+package emsi.moncef.order;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
