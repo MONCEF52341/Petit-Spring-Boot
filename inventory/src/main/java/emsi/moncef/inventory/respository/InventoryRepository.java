1829ac89646bc1399b37dbcb0be49ef746e1c4d9
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-27 21:01:28 +0100

    création du répo du module Inventory

---
+++ inventory/src/main/java/emsi/moncef/inventory/respository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package emsi.moncef.inventory.respository;
+
+import emsi.moncef.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
