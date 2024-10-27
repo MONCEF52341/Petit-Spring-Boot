fe34c81bf30e5378e489c6a83d3646e2c1199a03
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-27 21:01:36 +0100

    création du service du module Inventory

---
+++ inventory/src/main/java/emsi/moncef/inventory/service/InventoryService.java
@@ -0,0 +1,14 @@
+package emsi.moncef.inventory.service;
+
+import emsi.moncef.inventory.respository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+    public boolean isInStock(String skuCode,Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode,quantity);
+    }
+}
