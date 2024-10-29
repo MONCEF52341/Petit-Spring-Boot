9a5020ac702d41d693339694a98f6d1f61e2a85c
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-29 18:17:24 +0100

    ajout d'un script pour démarrer les MS

---
+++ service.sh
@@ -0,0 +1,20 @@
+
+# Utilisation : ./services.sh start|stop
+
+if [ "$1" == "start" ]; then
+    echo "Démarrage de tous les servicess..."
+    for dir in gateway product order inventory; do
+        echo "Lancement de $dir..."
+        (cd $dir && docker-compose up -d)
+    done
+    echo "Tous les services sont démarréés."
+elif [ "$1" == "stop" ]; then
+    echo "Arrêt de tous les services..."
+    for dir in gateway product order inventory; do
+        echo "Arrêt de $dir..."
+        (cd $dir && docker-compose down -v)
+    done
+    echo "Tous les services sont arrêtés."
+else
+    echo "Usage: $0 start|stop"
+fi
