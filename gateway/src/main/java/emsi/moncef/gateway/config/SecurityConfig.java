d2d4556e7bc961a4b951d3870192bc5f71a4f8db
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-10-31 18:42:16 +0100

    J'ai rassemblé la documentation swagger sur une seule page de la gateway

--- gateway/src/main/java/emsi/moncef/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/emsi/moncef/gateway/config/SecurityConfig.java
@@ -8,11 +8,26 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String[] authlessURLS = {
+      "/swagger-ui",
+      "/swagger-ui/**",
+      "/swagger-api/**",
+      "/v3/api-docs/**",
+      "/swagger-resources/**",
+      "/api-docs/**",
+      "/documentation/**"
+    };
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(authlessURLS)
+                        .permitAll()
+                        .anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
+
+
 }
--- gateway/src/main/java/emsi/moncef/gateway/routes/Routes.java
+++ gateway/src/main/java/emsi/moncef/gateway/routes/Routes.java
@@ -8,24 +8,49 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
     @Bean
-    public RouterFunction<ServerResponse> productServiceRoutes() {
+    public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
                 .build();
     }
     @Bean
-    public RouterFunction<ServerResponse> orderServiceRoutes() {
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/documentation/product/v3/swagger-api"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
         return GatewayRouterFunctions.route("order_service")
                 .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
                 .build();
     }
+
     @Bean
-    public RouterFunction<ServerResponse> inventoryServiceRoutes() {
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/documentation/order/v3/swagger-api"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/documentation/inventory/v3/swagger-api"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/v3/api-docs"))
                 .build();
     }
 }
