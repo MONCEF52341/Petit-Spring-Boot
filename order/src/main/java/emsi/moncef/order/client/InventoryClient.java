ee0f7f1e23f5594dd4298d9ca273996306d1473a
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-11-01 23:12:36 +0100

    j'ai rajouté un disjoncteur, resilience4j pour le module order

--- order/src/main/java/emsi/moncef/order/client/InventoryClient.java
+++ order/src/main/java/emsi/moncef/order/client/InventoryClient.java
@@ -1,9 +1,24 @@
 package emsi.moncef.order.client;
 
+
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+@Slf4j
 public interface InventoryClient {
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code,Integer quantity,Throwable throwable) {
+        log.info("L'inventaire est inaccessible pour le code {} , la raison étant : {}", code, throwable.getMessage());
+        return false;
+    }
 }
